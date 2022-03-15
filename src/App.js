71b6bf26b648ac4bb5ede3b7aad0999e58653f86
Robert Ward <raward4@ncsu.edu>
2022-03-15 01:03:04 -0400

    ads

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import FloorPlan from './FloorPlan'
+
+function App() {
+ 
+  return (
+    <div>
+      <FloorPlan 
+      // floorPlan={floorPlan}
+      />
+    </div>
+  )
+}
+export default App;
+
+// does not need array?? teacher teacher
